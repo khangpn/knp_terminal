08525f3cf707bf5b8e7c986d66ec96bd0e2fee7d
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2017-03-12 02:12:59 +0100

    Add jq to my_init; remove dropbox installation from my_init

--- my_init.sh
+++ my_init.sh
@@ -3,21 +3,23 @@ applications="
   vim
   git
   ssh
-  ubuntu-restricted-extras
-  samba
-  smbclient
   yakuake
   cmus
   ibus
   ibus-unikey
+  smplayer
+  ubuntu-restricted-extras
+  samba
+  smbclient
   xdotool
   cowsay
   fortune
   wallch
-  conky
   curl
   lm-sensors
   hddtemp
+  jq 
+  conky
 "
 for app in $applications
 do
@@ -28,6 +30,3 @@ do
         echo $app" has been installed"
     fi
 done
-
-curl -sSL https://get.rvm.io | bash -s stable --ruby
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
